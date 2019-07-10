e9f62e73051274d2de0113cfad1898e7ccc045aa
MasayukiOhashi <s1260109@u-aizu.ac.jp>
2019-07-10 15:32:05 +0900

    second revision

--- Diceroll.java
+++ Diceroll.java
@@ -1,10 +1,15 @@
 import java.util.*;
-class Diceroll{
+class Diceroll2{
 	public static void main(String[] args){
 		Random rand=new Random();
+		Scanner sc=new Scanner(System.in);
 		int a=0,b=0;
 		a=rand.nextInt(6)+1;
 		b=rand.nextInt(6)+1;
+		String name;
+		System.out.println("What is your name?");
+		name=sc.next();
+		System.out.println("Hello, "+name+"!");
 		System.out.println("Die 1: "+a);
 		System.out.println("Die 2: "+b);
 		System.out.println("Total value: "+(a+b));
